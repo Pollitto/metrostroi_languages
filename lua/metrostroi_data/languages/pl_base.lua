7a21b095ebe47c99afefd1d6ae21fa9eaa496cc0
Elkm <othjorkaw@gmail.com>
2018-06-03 16:17:20 +0200

    added polish base, emacs backup files to gitignore

---
+++ lua/metrostroi_data/languages/pl_base.lua
@@ -0,0 +1,272 @@
+return [[
+#Base text for Polish languagę
+
+[pl]
+lang        = Polish                            #Full language name
+AuthorText  = Autor przekładu: Elkm             #Author text
+
+#Workshop errors
+Workshop.Title              = Menadżer addonów
+Workshop.FilesMissing       = Brakuje niektórych plików addonu lub addon uszkodzony\nJeśli addon został zainstalowany za pośrednictwem Workshopu, spróbuj usunąć plik:\nGarrysMod/garrysmod/%s.
+Workshop.FilesMissingLocaly = Brakuje niektórych plików addonu lub addon uszkodzny.
+Workshop.InstalledLocaly    = Zainstalowany (lokalnie)
+Workshop.NotInstalledE      = Niezainstalowany.\nZasubskrybuj addon i sprawdź go w menu "Addony".
+Workshop.NotInstalled       = Niezainstalowany.
+Workshop.Disabled           = Wyłączony.\nWłącz go w menu "Addony".
+Workshop.Installed          = Zainstalowany.
+Workshop.Open               = Workshop
+Workshop.ErrorGithub        = Znaleziona wersja GitHub modu Metrostroi - jest ona niekompatybilna z wersją Legacy.
+Workshop.ErrorLegacy        = Znaleziona wersja Legacy modu Metrostroi - jest ona niekompatybilna z wersją GitHub.
+
+Workshop.Error1             = Ten addon zawiera stary kod modu Metrostroi, który nie jest kompatybilny z zainstalowanym. Mogą pojawić się "Script errors" i zachodzić niestabilność addonu.
+
+#Client settings
+Panel.Admin             = Admin
+Panel.RequireThirdRail  = Wymagaj trzeciej szyny
+
+Panel.Client            = Klient
+Panel.Language          = Wybierz język
+Panel.DrawCams          = Renderuj obraz z kamer
+Panel.DisableHUD        = Wyłącz HUD na miejscu maszynisty
+Panel.DisableCamAccel   = Wyłącz ruch kamery (wrażenie przyspieszenia)
+Panel.DisableHoverText  = Wyłącz podpowiedzi przycisków
+Panel.ScreenshotMode    = Tryb fotografii (NISKI FPS!)
+Panel.ShadowsHeadlight  = Włącz dynamiczne cienie (lampy)
+Panel.ShadowsOther      = Włącz dynamiczne cienie (inne źródła)
+Panel.MinimizedShow     = Nie zwalniaj zasobów podczas minimalizacji
+Panel.FOV               = FOV (pole widzenia)
+Panel.RenderDistance    = Odległość rysowania
+Panel.ReloadClient      = Przeładuj zasoby (klient)
+
+Panel.ClientAdvanced    = Klient (zaawansowane)
+Panel.DrawDebugInfo     = Pokaż dodatkowe informacje (debug)
+Panel.CheckAddons       = Przeskanuj addony
+Panel.ReloadLang        = Przeładuj języki
+Panel.SoftDraw          = Miękkie rysowanie ??
+Panel.SoftReloadLang    = Nie przeładowywuj spawnmenu
+
+#Common train
+Train.Common.Camera0        = Miejsce maszynisty
+Train.Common.RouteNumber    = Numer brygady
+Train.Common.LastStation    = Stacja końcowa
+Train.Common.HelpersPanel   = Panel pomocnika maszynisty
+Common.ARM.Monitor1         = Monitor ARM 1
+Train.Buttons.Sealed        = Zaplombowane
+
+#Train entities
+Entities.gmod_subway_base.Name        = Baza pociągu metra
+Entities.gmod_subway_81-502.Name      = 81-502 (Ema-502 końcowy)
+Entities.gmod_subway_81-501.Name      = 81-501 (Em-501 środkowy)
+Entities.gmod_subway_81-702.Name      = 81-702 (D końcowy)
+Entities.gmod_subway_81-702_int.Name  = 81-702 (D środkowy)
+Entities.gmod_subway_81-703.Name      = 81-703 (E końcowy)
+Entities.gmod_subway_81-703_int.Name  = 81-703 (E środkowy)
+Entities.gmod_subway_ezh.Name         = 81-707 (Ezh końcowy)
+Entities.gmod_subway_ezh1.Name        = 81-708 (Ezh1 środkowy)
+Entities.gmod_subway_ezh3.Name        = 81-710 (Ezh3 końcowy)
+Entities.gmod_subway_em508t.Name      = 81-508T (Em-508T środkowy)
+Entities.gmod_subway_81-717_mvm.Name  = 81-717 (moskiewski, końcowy)
+Entities.gmod_subway_81-714_mvm.Name  = 81-714 (moskiewski, środkowy)
+Entities.gmod_subway_81-717_lvz.Name  = 81-717 (petersburski, końcowy)
+Entities.gmod_subway_81-714_lvz.Name  = 81-714 (petersburski, środkowy)
+Entities.gmod_subway_81-718.Name      = 81-718 (TISU końcowy)
+Entities.gmod_subway_81-719.Name      = 81-719 (TISU środkowy)
+Entities.gmod_subway_81-720.Name      = 81-720 (Yauza końcowy)
+Entities.gmod_subway_81-721.Name      = 81-721 (Yauza środkowy)
+Entities.gmod_subway_81-722.Name      = 81-722 (Yubileyniy końcowy)
+Entities.gmod_subway_81-723.Name      = 81-723 (Yubileyniy środkowy silnikowy)
+Entities.gmod_subway_81-724.Name      = 81-724 (Yubileyniy środkowy toczny)
+Entities.gmod_subway_81-7036.Name     = 81-7036 (nie działa)
+Entities.gmod_subway_81-7037.Name     = 81-7037 (nie działa)
+Entities.gmod_subway_tatra_t3.Name    = Tatra T3
+
+#Train util entities
+Entities.gmod_train_bogey.Name        = Wózek
+Entities.gmod_train_couple.Name       = Sprzęg
+
+#Other entities
+Entities.gmod_track_pui.Name                = PUI
+Entities.gmod_track_mus_elektronika7.Name   = Zegar "Electronika"
+Entities.gmod_mus_clock_analog.Name         = Zegar analogowy
+Entities.gmod_track_clock_time.Name         = Duży stoper (czas)
+Entities.gmod_track_clock_small.Name        = Mały stoper
+Entities.gmod_track_clock_interval.Name     = Duży stoper (interwał)
+Entities.gmod_track_switch.Name             = Zwrotnica (kontroler)
+Entities.gmod_track_powermeter.Name         = Kilowatomierz
+Entities.gmod_track_arm.Name                = Pulpit nastawczy
+Entities.gmod_track_udochka.Name            = Konektor
+Entities.gmod_train_spawner.Name            = Spawner
+
+#Spawner common
+Common.Spawner.Texture      = Malowanie
+Common.Spawner.PassTexture  = Wnętrze
+Common.Spawner.CabTexture   = Kabina
+Common.Spawner.Announcer    = Zapowiedzi
+Common.Spawner.Type1        = Typ 1
+Common.Spawner.Type2        = Typ 2
+Common.Spawner.Type3        = Typ 3
+Common.Spawner.Type4        = Typ 4
+
+
+#Trains common
+Common.ALL.Unsused1                         = Nieużywane
+Common.ALL.Unsused2                         = (nieużywane)
+Common.ALL.VB                               = VB: Wyłącznik baterii akumulatorów
+Common.ALL.VSOSD                            = SOSD: Wyłącznik kontrolki otwarcia drzwi stacyjnych
+Common.ALL.VKF                              = VKF: Wyłącznik bateryjnego zasilania reflektorów czerwonych
+Common.ALL.VB2                              = (Obwody pomocnicze NN)
+Common.ALL.VPR                              = VPR: Wyłącznik radiotelefonu
+Common.ALL.VASNP                            = Wyłącznik ASNP
+Common.ALL.UOS                              = RC-UOS: Urządzenie ograniczenia prędkości (jazda bez EPV/EPK)
+Common.ALL.VAH                              = VAH: Wyłącznik jazdy awaryjnej
+Common.ALL.KAH                              = KAH: Przycisk jazdy awaryjnej bez ARS
+Common.ALL.VAD                              = VAD: Wyłącznik awaryjny drzwi
+Common.ALL.OVT                              = OVT: Odłączenie hamulców pneumatycznych
+Common.ALL.VOVT                             = VOVT: Wyłącznik odłączenia hamulców pneumatycznych
+Common.ALL.EmergencyBrakeValve              = Hamulec bezpieczeństwa
+Common.ALL.VU                               = VU: Wyłącznik sterowania
+Common.ALL.KDP                              = KDP: Otwarcie drzwi prawych
+Common.ALL.KDPK                             = Klapka przycisku drzwi prawych
+Common.ALL.KDL                              = KDL: Otwarcie drzwi lewych
+Common.ALL.KDLK                             = Klapka przycisku drzwi prawych
+Common.ALL.KDLPK                            = Klapka przycisków drzwi
+Common.ALL.KRZD                             = KRZD: Rezerwowe zamykanie drzwi
+Common.ALL.Ring                             = Dzwonek
+Common.ALL.VUD                              = VUD: Zamykanie drzwi
+Common.ALL.KDPH                             = Otwarcie prawych drzwi ostatniego wagonu
+Common.ALL.VUD2                             = VUD2: Zamykanie drzwi od strony pomocnika
+Common.ALL.Program1                         = Program I
+Common.ALL.Program2                         = Program II
+Common.ALL.VRP                              = VRP: Odblokowanie RP
+Common.ALL.KSN                              = KSN: Sygnał awarii
+Common.ALL.VMK                              = VMK: Sprężarka
+Common.ALL.VF1                              = Wyłącznik pierwszej grupy reflektorów
+Common.ALL.VF2                              = Wyłącznik drugiej grupy reflektorów
+Common.ALL.VF                               = Wyłącznik reflektorów
+Common.ALL.VUS                              = VUS: Wyłącznik silniejszego światła reflektorów
+Common.ALL.GaugeLights                      = Oświetlenie pulpitu
+Common.ALL.CabLights                        = Oświetlenie kabiny
+Common.ALL.RMK                              = RMK: Rezerwowe sterowanie sprężarki
+Common.ALL.KRP                              = KRP: Rozruch rezerwowy
+Common.ALL.VZP                              = VZP: Wyłącznik wstrzymania odjazdu systemu jazdy samoczynnej
+Common.ALL.VZD                              = VZD: Wyłącznik wstrzymania drzwi
+Common.ALL.VAV                              = VAV: Wyłącznik jazdy samoczynnej
+Common.ALL.RouteNumber1+                    = Zwiększenie pierwszej cyfry numeru brygady
+Common.ALL.RouteNumber1-                    = Zmniejszenie pierwszej cyfry numeru brygady
+Common.ALL.RouteNumber2+                    = Zwiększenie drugiej cyfry numeru brygady
+Common.ALL.RouteNumber2-                    = Zmniejszenie drugiej cyfry numeru brygady
+Common.ALL.RouteNumber3+                    = Zwiększenie trzeciej cyfry numeru brygady
+Common.ALL.RouteNumber3-                    = Zmniejszenie trzeciej cyfry numeru brygady
+Common.ALL.LastStation+                     = Przewinięcie tablicy stacji końcowych naprzód
+Common.ALL.LastStation-                     = Przewinięcie tablicy stacji końcowych w tył
+Common.ALL.RRP                              = Czerwona kontrolka RP (brak załączenia obwodu głównego)
+Common.ALL.GRP                              = Zielona kontrolka RP
+Common.ALL.RP                               = RP: Czerwona kontrolka RP (zadziałanie RP na jednym z wagonów lub brak załączenia obwodu głównego)
+Common.ALL.SN                               = LSN: Czerwona kontrolka RP (brak załączenia obwodu głównego jednego lub więcej wagonów)
+Common.ALL.PU                               = Kontrolka niskiego rozruchu
+Common.ALL.BrT                              = Kontrolka działania hamulca pneumatycznego pociągu
+Common.ALL.ARS                              = ARS: Wyłącznik systemu ARS
+Common.ALL.ARSR                             = ARS-R: Wyłącznik pracy systemu ARS w trybie ARS-R
+Common.ALL.ALS                              = ALS: Wyłącznik ALS
+Common.ALL.RCARS                            = RC-1: Odłącznik obwodów ARS
+Common.ALL.EPK                              = EPK: elektropneumatyczny zawór ARS
+Common.ALL.EPV                              = EPV: elektropneumatyczny zawór ARS
+Common.ARS.LN                               = LN: Kontrolka jazdy w kierunku właściwym
+Common.ARS.KT                               = KT: Kontrolka hamowania
+Common.ARS.VD                               = VD: Kontrolka wyłączenia
+Common.ARS.Freq                             = Przełączenie trybu pracy ALS
+Common.ARS.FreqD                            = (w górę 1/5, w dół 2/6)
+Common.ARS.FreqU                            = (w górę 2/6, w dół 1/5)
+Common.ARS.RS                               = RS: Lampa równości ograniczeń prędkości
+Common.ARS.AB                               = Praca systemu ARS w trybie blokady liniowej
+Common.ARS.ABButton                         = Przycisk przejścia w tryb blokady liniowej
+Common.ARS.ABDriver                         = (maszynisty)
+Common.ARS.ABHelper                         = (pomocnika)
+Common.ARS.AV                               = Kontrolka awarii bloku głównego systemu ARS-MP
+Common.ARS.AV1                              = Kontrolka awarii bloku rezerwowego systemu ARS-MP
+Common.ARS.AB2                              = Przycisk przejścia w tryb blokady liniowej
+Common.ARS.ARS                              = Zasadniczy tryb pracy systemu ARS
+Common.ARS.LRD                              = LRD: Kontrolka zezwolenia jazdy podczas sygnału "stój"
+Common.ARS.VRD                              = VRD: Zezwolenie jazdy podczas sygnału "stój"
+Common.ARS.KB                               = KB: Przycisk czuwaka
+Common.ARS.KVT                              = KVT: Przycisk kasowania hamowania
+Common.ARS.KVTR                             = KVT: Przycisk kasowania hamowania w trybie ARS-R
+Common.ARS.04                               = OCh: Brak sygnału ARS
+Common.ARS.0                                = 0: Sygnał "stój"
+Common.ARS.40                               = 40: Ograniczenie 40 km/h
+Common.ARS.60                               = 60: Ograniczenie 60 km/h
+Common.ARS.70                               = 70: Ograniczenie 70 km/h
+Common.ARS.80                               = 80: Ograniczenie 80 km/h
+Common.ALL.RCBPS                            = RC-BPS: Odłącznik obwodów systemu przeciwpoślizgowego
+Common.BPS.On                               = Działanie systemu przeciwpoślizgowego
+Common.BPS.Err                              = Błąd systemu przeciwpoślizgowego
+Common.BPS.Fail                             = Awaria systemu przeciwpoślizgowego
+Commom.NMnUAVA.NMPressureLow                = Kontrolka niskiego ciśnienia w przewodzie zasilającym
+Commom.NMnUAVA.UAVATriggered                = Kontrolka otwarcia styków UAVA
+Common.ALL.L1w                              = Kontrolka przewodu 1. (załączenie obwodu głównego - rozruch)
+Common.ALL.L2w                              = Kontrolka przewodu 2. (praca kontrolera reostatu (PSR))
+Common.ALL.L6w                              = Kontrolka przewodu 6. (załączenie obwodu głównego - hamowanie ED)
+Common.ALL.DriverValveBLDisconnect          = Zawór dwudrożny przewodu głównego
+Common.ALL.DriverValveTLDisconnect          = Zawór dwudrożny przewodu zasilającego
+Common.ALL.FrontBrakeLineIsolationToggle    = Zawór przewodu głównego sprzęgu powietrznego
+Common.ALL.FrontTrainLineIsolationToggle    = Zawór przewodu zasilającego sprzęgu powietrznego
+Common.ALL.RearBrakeLineIsolationToggle     = Zawór przewodu głównego sprzęgu powietrznego
+Common.ALL.RearTrainLineIsolationToggle     = Zawór przewodu zasilającego sprzęgu powietrznego
+Common.ALL.UAVA                             = UAVA: Włączenie samoczynnego wyłącznika autostopu\n(możliwe dopiero po zmniejszeniu ciśnienia w przewodzie głównym)
+Common.ALL.UAVAContact                      = Przywrócenie styków UAVA
+Common.ALL.OAVU                             = OAVU: Wyłącznik odłączenia AVU
+Common.ALL.LAVU                             = Kontrolka działania AVU
+Common.ALL.GV                               = GV: Główny odłącznik
+Common.ALL.AirDistributor                   = VRN: Wyłącznik zaworu rozrządrzego
+Common.ALL.CabinDoor                        = Drzwi do kabiny
+Common.ALL.FrontDoor                        = Drzwi przednie
+Common.ALL.RearDoor                         = Drzwi tylne
+
+Common.ALL.UNCh                             = UNCh: Włączenie wzmacniacza niskich częstotliwości
+Common.ALL.ES                               = ES: Przełącznik kontroli łączności alarmowej
+Common.ALL.GCab                             = Włączenie głośnika w kabinie
+Common.ALL.UPO                              = UPO: Włączenie urządzeń rozgłaszania komunikatów dźwiękowych
+Common.ALL.R_Radio                          = Włączenie systemu zapowiedzi głosowych
+Common.ALL.AnnPlay                          = Kontrolka odtwarzania zapowiedzi głosowej
+#RRI
+Train.Common.RRI                            = RRI: Przekaźnikowy system zapowiedzi głosowych
+Common.RRI.RRIUp                            = RRI: Konfiguracja w górę
+Common.RRI.RRIDown                          = RRI: Konfiguracja w dół
+Common.RRI.RRILeft                          = RRI: Konfiguracja w lewo
+Common.RRI.RRIRight                         = RRI: Konfiguracja w prawo
+Common.RRI.RRIEnableToggle                  = RRI: Zasilanie
+Common.RRI.RRIRewindSet2                    = RRI: Przewijanie do przodu
+Common.RRI.RRIRewindSet0                    = RRI: Przewijanie do tyłu
+Common.RRI.RRIAmplifierToggle               = RRI: Wzmacniacz
+Common.RRI.RRIOn                            = RRI: Kontrolka pracy RRI 
+#ASNP
+Train.Common.ASNP           = ASNP
+Common.ASNP.ASNPMenu        = ASNP: Menu
+Common.ASNP.ASNPUp          = ASNP: W górę
+Common.ASNP.ASNPDown        = ASNP: W dół
+Common.ASNP.ASNPOn          = Wyłącznik ASNP
+
+#IGLA
+Train.Common.IGLA           = IGLA
+Common.IGLA.Button1Up       = IGLA: Pierwszy przycisk w górę
+Common.IGLA.Button1         = IGLA: Pierwszy przycisk
+Common.IGLA.Button1Down     = IGLA: Pierwszy przycisk w dół
+Common.IGLA.Button2Up       = IGLA: Drugi przycisk w górę
+Common.IGLA.Button2         = IGLA: Drugi przycisk
+Common.IGLA.Button2Down     = IGLA: Drugi przycisk w górę
+Common.IGLA.IGLASR          = IGLA: Zasilanie
+Common.IGLA.IGLARX          = IGLA: Brak łączności
+Common.IGLA.IGLAErr         = IGLA: Błąd
+Common.IGLA.IGLAOSP         = IGLA: Zadziałanie systemu przeciwpożarowego
+Common.IGLA.IGLAPI          = IGLA: Pożar
+Common.IGLA.IGLAOff         = IGLA: Odłączenie obwodów WN
+#Train helpers common
+
+Common.ALL.Speedometer      = Prędkościomierz
+Common.ALL.BLTLPressure     = Ciśnienie w przewodach: głównym (wsk. czerwona) i zasilającym (wsk. czarna)
+Common.ALL.BCPressure       = Ciśnienie w cylindrach hamulcowych
+Common.ALL.EnginesCurrent   = Prąd trakcyjny [A]
+Common.ALL.EnginesVoltage   = Napięcie trakcyjne [kV]
+Common.ALL.BatteryVoltage   = Napięcie baterii [V]
+Common.ALL.HighVoltage      = Woltomierz WN [kV]
+]]
